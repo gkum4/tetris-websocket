9c59f071cef543ce8938de95955cc67328858385
Gustavo Kumasawa <gustavokumasawa@gmail.com>
2021-03-23 17:12:45 -0300

    Fix interval and gameStatus

--- src/hooks/useGameStatus.js
+++ src/hooks/useGameStatus.js
@@ -7,18 +7,17 @@ export const useGameStatus = rowsCleared => {
 
   const linePoints = useMemo(() => [40, 100, 300, 1200], []);
 
-  const calcScore = useCallback(() => {
-    // We have score
+  const calculateScore = useCallback(() => {
     if (rowsCleared > 0) {
-      // This is how original Tetris score is calculated
+      
       setScore(prev => prev + linePoints[rowsCleared - 1] * (level + 1));
       setRows(prev => prev + rowsCleared);
     }
   }, [level, linePoints, rowsCleared]);
 
   useEffect(() => {
-    calcScore();
-  }, [calcScore, rowsCleared, score]);
+    calculateScore();
+  }, [calculateScore, rowsCleared, score]);
 
   return [score, setScore, rows, setRows, level, setLevel];
 };
