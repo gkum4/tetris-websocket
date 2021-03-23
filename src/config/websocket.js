a86034da3597118a658712b63fd2f2790265c2bb
Gustavo Kumasawa <gustavokumasawa@gmail.com>
2021-03-23 15:57:41 -0300

    Feat websocket config

--- src/config/websocket.js
+++ src/config/websocket.js
@@ -6,22 +6,9 @@ const wss = new WebSocket.Server({ port: 8888 });
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
     if (message === '1') {
-      robot.typeString('`');
-    }
-    if (message === '2') {
-      robot.typeString('`');
-    }
-    if (message === '3') {
-      robot.typeString('`');
-    }
-    if (message === '4') {
-      robot.typeString('`');
-    }
-    if (message === '5') {
-      robot.typeString('`');
-    }
-    if (message === '6') {
-      robot.typeString('`');
+      robot.keyTap('right');
     }
   });
 });
+
+console.log('started')
